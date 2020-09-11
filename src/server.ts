0da96d5fb1d5d20bdff58d403f864841885ea0ae
Breno Castleo Branco <breno.cbsousa@gmail.com>
2020-09-11 20:37:17 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -9,6 +10,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
